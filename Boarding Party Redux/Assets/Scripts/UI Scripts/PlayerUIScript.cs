4e1a831ab17d7c18d4e0974211a36ad941b261e0
Artemis <Horodotion@live.ie>
2022-07-20 02:16:52 -0700

    UI-Beginning

---
+++ Boarding Party Redux/Assets/Scripts/UI Scripts/PlayerUIScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUIScript : MonoBehaviour
+{
+    public Image playerImage;
+    public Slider healthbar;
+
+    public void InitializePlayerUI()
+    {
+
+    }
+
+    public void SetHealth(float newHealth)
+    {
+
+    }
+}
